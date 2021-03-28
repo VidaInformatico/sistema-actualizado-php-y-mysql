1b83786a3d5398e573d6a83c4255c2c971deefd7
Angel Sifuentes <71534078+VidaInformatico@users.noreply.github.com>
2021-03-27 19:17:15 -0500

    Update empresa.php

--- sistema/empresa.php
+++ sistema/empresa.php
@@ -7,8 +7,7 @@ $txtRSocial = $_POST['txtRSocial'];
 $txtTelefono = $_POST['txtTelEmpresa'];
 $txtDireccion = $_POST['txtDirEmpresa'];
 $txtemail = $_POST['txtEmailEmpresa'];
-$txtigv = $_POST['txtIgv'];
-$actualizar_empresa = mysqli_query($conexion, "UPDATE configuracion SET dni = $txtdni, nombre = '$txtNombre', razon_social = '$txtRSocial', telefono = $txtTelefono, email = '$txtemail', direccion = '$txtDireccion', igv = $txtigv");
+$actualizar_empresa = mysqli_query($conexion, "UPDATE configuracion SET dni = $txtdni, nombre = '$txtNombre', razon_social = '$txtRSocial', telefono = $txtTelefono, email = '$txtemail', direccion = '$txtDireccion'");
 mysqli_close($conexion);
 if ($actualizar_empresa) {
   $alert = '<p class="msg_save">Configuración de empresa Actualizado</p>';
