b5ce6b5ab7d90f4021879def8f8574e198233dbb
Angel Sifuentes <71534078+VidaInformatico@users.noreply.github.com>
2021-04-04 20:07:58 -0500

    Update index.php

--- index.php
+++ index.php
@@ -102,12 +102,12 @@ if (!empty($_SESSION['active'])) {
   </div>
 
   <!-- JavaScript files-->
-    <script src="vendor/jquery/jquery.min.js"></script>
-    <script src="vendor/popper.js/umd/popper.min.js"> </script>
-    <script src="vendor/bootstrap/js/bootstrap.min.js"></script>
-    <script src="vendor/jquery.cookie/jquery.cookie.js"> </script>
-    <script src="vendor/jquery-validation/jquery.validate.min.js"></script>
-    <script src="js/front.js"></script>
+    <script src="sistema/vendor/jquery/jquery.min.js"></script>
+    <script src="sistema/vendor/popper.js/umd/popper.min.js"> </script>
+    <script src="sistema/vendor/bootstrap/js/bootstrap.min.js"></script>
+    <script src="sistema/vendor/jquery.cookie/jquery.cookie.js"> </script>
+    <script src="sistema/vendor/jquery-validation/jquery.validate.min.js"></script>
+    <script src="sistema/js/front.js"></script>
 
 
 </body>
